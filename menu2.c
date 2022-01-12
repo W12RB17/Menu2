8a5cab61d2a2a6613f20d9dbed230746f1a114a4
Alejandro Franco <Alejandro.franco@teaminternational.com>
2022-01-12 11:04:09 -0500

    Four options Working

--- menu2.c
+++ menu2.c
@@ -22,7 +22,8 @@ int main(void)
         \n2. Convert a string to uppercase letters.\
         \n3. Alternate letters of a string between upper and lowercase letters.\
         \n4. Replace a letter of a string.\n5. Execute Command line.\
-        \n5. to Exit\
+        \n5. Command line\
+        \n6. To exit.\
         \n\nR/: ");
 
         scanf("%d", &answer);
@@ -72,7 +73,7 @@ int main(void)
                     printf("%c", toupper(string2[i]));
                 }
                 printf("\n\n");
-                printf("[0] to go back to main menu or [1] if you want to convert another string to lowercase letters...\
+                printf("[0] to go back to main menu or [1] if you want to convert another string to uppercase letters...\
                 \n\nR/:");
                 scanf("%i", &question);
                 if (question == 1)
@@ -109,7 +110,7 @@ int main(void)
                     }
                 }
                 printf("\n\n");
-                printf("[0] to go back to main menu or [1] if you want to convert another string to lowercase letters...\
+                printf("[0] to go back to main menu or [1] if you want to try with other string...\
                 \n\nR/:");
                 scanf("%i", &question);
                 if (question == 1)
@@ -124,9 +125,45 @@ int main(void)
         }
         else if (answer == 4)
         {
-            printf("you have selected option 4\n");
+            while (true)
+            {
+                scanf("%c", &temp);
+                char string4[50];
+                system("clear");
+                printf("Type a string, the letter that you want to replace and the character to replace it without spaces (string,letter,character): ");
+                scanf("%[^\n]", string4);
+                char * string = strtok(string4, ",");
+                char * letter = strtok(NULL,",");
+                char * character = strtok(NULL, ",");
+                printf("\nYour new string is:, the lenght of array is: ");
+
+                for (int i = 0; i < strlen(string4); i++)
+                {   
+                    if (string4[i] == *letter)
+                    {
+                        printf("%c", (string4[i] = *character));
+                    }
+                    else
+                    {
+                        printf("%c", string4[i]);
+                    }
+                }
+
+                printf("\n\n");
+                printf("[0] to go back to main menu or [1] if you want to try with other string...\
+                \n\nR/:");
+                scanf("%i", &question);
+                if (question == 1)
+                {
+                    continue;
+                }
+                else if (question == 0)
+                {
+                    break;
+                }
+            }
         }
-        else if (answer == 5)
+        else if (answer == 6)
         {
             system("clear");
             printf("Enjoy your life!!\n\n");
